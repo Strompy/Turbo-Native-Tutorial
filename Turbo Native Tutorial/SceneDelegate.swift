e9015c7d5b8ab70f0b1c63cfea256b90b1eff160
Phillip Strom <phillip.strom@gmail.com>
2024-03-29 10:23:02 -0600

    Add navigator

--- Turbo Native Tutorial/SceneDelegate.swift
+++ Turbo Native Tutorial/SceneDelegate.swift
@@ -13,10 +13,13 @@ let baseURL = URL(string: "https://turbo-native-demo.glitch.me/")!
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    private let navigator = TurboNavigator()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        window?.rootViewController = navigator.rootViewController
+        navigator.route(baseURL)
     }
 }
 
